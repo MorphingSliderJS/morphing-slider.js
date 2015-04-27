e544542200550896add15da78f82ec02c125507c
yamamoto nodoka <mild.summer.y@gmail.com>
2015-04-27 11:09:44 +0900

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,15 @@
 var gulp = require('gulp'),
     babel = require('gulp-babel'),
     sass = require('gulp-sass'),
-    server = require('gulp-webserver'),
-    MockServer = require('easymock').MockServer;
+    server = require('gulp-webserver');
 
-gulp.task('default', function() {
-    return gulp.src(['src/js/*.js'])
+gulp.task('js', function() {
+    return gulp.src(['src/js/*.es6'])
         .pipe(babel())
         .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('easymock', function () {
-    var ms = new MockServer({
-        keepalive: true,
-        port: 3000,
-        path: './json',
-    });
-    ms.start();
-});
-
-gulp.task('server', ['mock'], function() {
+gulp.task('server', function() {
     gulp.src('build')
         .pipe(server({
         livereload: true,
@@ -33,18 +23,18 @@ gulp.task('server', ['mock'], function() {
 
 gulp.task('html', function () {
     gulp.src('./src/*.html')
-        .pipe(connect.reload());
+        .pipe(gulp.dest('./src/'));
 });
 
 gulp.task('sass', function () {
     gulp.src('./src/sass/*.scss')
         .pipe(sass({includePaths: ['./styles'],
                     errLogToConsole: true}))
-        .pipe(gulp.dest('./build/css'))
-        .pipe(connect.reload());
+        .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('watch', ['sass', 'html'], function () {
+    gulp.watch(['./src/js/*.es6'], ['js']);
     gulp.watch(['./src/*.html'], ['html']);
     gulp.watch(['./src/sass/*.scss'], ['sass']);
 });
